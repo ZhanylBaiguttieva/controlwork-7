71e9a127cfbf5d6dc25c00745ed41acb29bef999
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-11-04 17:32:57 +0600

    Added app.css and app.tsx in container directory

--- src/container/App.tsx
+++ src/container/App.tsx
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 import './App.css';
-import {Item, Position} from './types';
+import {Item, Position} from '../types';
 
 function App() {
   const items: Item[] = [
